7a7b2b89198a4e6fafdc8d5dc4c75936a7e6b8f3
Carlos <carlodiaz@umariana.edu.co>
2022-03-12 18:00:30 -0500

    creada la funcion que permite registrar aun usuario de firebase

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,2 +1,5 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { AUTH } from "@/firebase/config";
+
+export const CREAR_CUENTA = async (correo: string, password: string) =>
+  await createUserWithEmailAndPassword(AUTH, correo, password);
