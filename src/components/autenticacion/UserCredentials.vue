de4bcad07f1ec37103d533b2b4d285ddafe4823a
Carlos <carlodiaz@umariana.edu.co>
2022-03-15 18:21:01 -0500

    Se agrego una validación genérica para los forms, usando vee-validate

--- src/components/autenticacion/UserCredentials.vue
+++ src/components/autenticacion/UserCredentials.vue
@@ -1,37 +1,63 @@
 <template>
   <v-card-text>
-    <v-form class="my-2">
-      <v-text-field
-        outlined
-        dense
-        label="Correo eléctronico"
-        prepend-icon="mdi-account"
-        counter
-      />
-      <v-text-field
-        outlined
-        dense
-        label="Contraseña"
-        prepend-icon="mdi-lock"
-        counter
-        :append-icon="mostrarPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        @click:append="mostrarPassword = !mostrarPassword"
-        :type="mostrarPassword ? 'text' : 'password'"
-      />
-      <v-text-field
-        outlined
-        dense
-        label="Repita la contraseña"
-        prepend-icon="mdi-lock"
-        counter
-        :append-icon="mostrarPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        @click:append="mostrarPassword = !mostrarPassword"
-        :type="mostrarPassword ? 'text' : 'password'"
-      />
-      <v-btn block color="success">
-        Registrarme <v-icon>mdi-account-plus</v-icon>
-      </v-btn>
-    </v-form>
+    <ValidationObserver ref="observer" v-slot="{ invalid }">
+      <v-form class="my-2">
+        <validation-provider
+          v-slot="{ errors }"
+          name="Correo eléctronico"
+          rules="required|email"
+        >
+          <v-text-field
+            outlined
+            dense
+            label="Correo eléctronico"
+            prepend-icon="mdi-account"
+            counter
+            :error-messages="errors"
+            v-model="correo"
+          />
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="Contraseña"
+          rules="required|min:8|max:60"
+        >
+          <v-text-field
+            outlined
+            dense
+            label="Contraseña"
+            prepend-icon="mdi-lock"
+            counter
+            :append-icon="mostrarPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="mostrarPassword = !mostrarPassword"
+            :type="mostrarPassword ? 'text' : 'password'"
+            :error-messages="errors"
+            v-model="pass1"
+          />
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="Contraseña"
+          rules="required|min:8|max:60"
+        >
+          <v-text-field
+            outlined
+            dense
+            label="Repeta la contraseña"
+            prepend-icon="mdi-lock"
+            counter
+            :append-icon="mostrarPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="mostrarPassword = !mostrarPassword"
+            :type="mostrarPassword ? 'text' : 'password'"
+            :error-messages="errors"
+            v-model="pass2"
+          />
+        </validation-provider>
+        <v-btn block color="success" :disabled="invalid">
+          Registrarme <v-icon>mdi-account-plus</v-icon>
+        </v-btn>
+      </v-form>
+    </ValidationObserver>
   </v-card-text>
 </template>
 
@@ -42,6 +68,9 @@ export default Vue.extend({
   name: "UserCredentials",
   data: () => ({
     mostrarPassword: false,
+    correo: "",
+    pass1: "",
+    pass2: "",
   }),
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,34 @@ import vuetify from "./plugins/vuetify";
 import "@babel/polyfill";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
+// ---------------------- REGLAS VALIDACIÓN CAMPOS ----------------------
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { required, digits, email, max, min } from "vee-validate/dist/rules";
+import { extend } from "vee-validate";
+
+Vue.component("ValidationObserver", ValidationObserver);
+Vue.component("ValidationProvider", ValidationProvider);
+
+extend("digits", {
+  ...digits,
+  message: "{_field_}: Se necesita {length} digitos. ({_value_})",
+});
+extend("required", {
+  ...required,
+  message: "{_field_}: no puede estar vacio",
+});
+extend("max", {
+  ...max,
+  message: "{_field_}: cantidad de caracteres superada, maximo {length}",
+});
+extend("min", {
+  ...min,
+  message: "{_field_}: requiere mas caracteres, minimo {length}",
+});
+extend("email", {
+  ...email,
+  message: "Correo con formato incorrecto",
+});
 
 Vue.config.productionTip = false;
 
