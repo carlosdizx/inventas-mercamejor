91e06a34d552453f273f91415eca989599f79cd6
Carlos <carlodiaz@umariana.edu.co>
2022-03-18 16:03:32 -0500

    Creado componente que se comunica con el toolbar para mostrar información de acuerdo a las acciones del usuario

---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-navigation-drawer v-model="nav" relative app>
+    <v-list nav dense>
+      <h1>Item 1</h1>
+      <h1>Item 2</h1>
+      <h1>Item 3</h1>
+      <h1>Item 4</h1>
+      <h1>Item 5</h1>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { mapState } from "vuex";
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NavDrawer",
+  data: () => ({
+    element: null,
+    nav: false,
+  }),
+  computed: {
+    ...mapState(["color"]),
+  },
+  methods: {
+    changeElement(idItem: any) {
+      this.$emit("changeDrawer", idItem);
+    },
+    change() {
+      this.nav = !this.nav;
+    },
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,26 +5,39 @@
       permitirColor
       :titulo="'Inventas - ' + nombre"
       :botones="botones"
+      @cambiarEstadoNavDrawer="changeStateBar"
     />
+    <NavDrawer ref="NavDrawer" />
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
 import Toolbar from "@/components/generals/Toolbar.vue";
+import NavDrawer from "@/components/NavDrawer.vue";
 
 export default Vue.extend({
   name: "Dashboard",
-  components: { Toolbar },
+  components: { Toolbar, NavDrawer },
   data: () => ({
     item: 0,
-    navigation: false,
+    navigation: true,
     nombre: "Mercamejor",
     botones: [
       { icon: "mdi-airplane", link: "registro" },
       { icon: "mdi-pipe", link: "/" },
     ],
   }),
+  methods: {
+    itemSelector(idItem) {
+      this.item = idItem;
+      this.navigation = false;
+    },
+    changeStateBar() {
+      this.navigation = !this.navigation;
+      this.$refs.NavDrawer.change();
+    },
+  },
 });
 </script>
 
