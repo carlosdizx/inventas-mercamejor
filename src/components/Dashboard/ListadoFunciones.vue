18dfd773d9ffcc7eb3e4f3a7e548d96ebe1f6544
Carlos <carlodiaz@umariana.edu.co>
2022-03-19 23:34:02 -0500

    desacoplado los datos del listado en un archivo dedicado para contener la información

--- src/components/Dashboard/ListadoFunciones.vue
+++ src/components/Dashboard/ListadoFunciones.vue
@@ -2,7 +2,7 @@
   <div>
     <div v-for="(item, index) of lista" :key="index">
       <v-alert v-if="item.tipo === 1" :color="item.color" dark dense>
-        Estudiantes
+        {{ item.titulo }}
       </v-alert>
       <v-list-item link v-if="item.tipo === 2">
         <v-list-item-icon>
@@ -16,20 +16,16 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { FUNCIONES } from "@/generals/funcionalidades";
 
 export default Vue.extend({
   name: "ListadoFunciones",
   data: () => ({
-    lista: [
-      { tipo: 1, titulo: "Básicos", color: "deep-purple darken-1" },
-      { tipo: 2, icono: "mdi-airplane", titulo: "Avión" },
-      { tipo: 1, titulo: "Intermedio", color: "indigo" },
-      { tipo: 2, icono: "mdi-home", titulo: "Home" },
-      { tipo: 1, titulo: "Avanzado", color: "amber" },
-      { tipo: 2, icono: "mdi-account", titulo: "Cuentas" },
-      { tipo: 2, icono: "mdi-account-plus", titulo: "Agregar cuentas" },
-    ],
+    lista: [{}],
   }),
+  created() {
+    this.lista = FUNCIONES;
+  },
 });
 </script>
 
---
+++ src/generals/funcionalidades.ts
@@ -0,0 +1,18 @@
+export const FUNCIONES: any[] = [
+  {
+    tipo: 1,
+    titulo: "Modulo de compras",
+    color: "success darken-1",
+  },
+  { id: 1, tipo: 2, icono: "mdi-cart-arrow-up", titulo: "Compras" },
+  { id: 2, tipo: 2, icono: "mdi-account-group", titulo: "Proveedores" },
+  { id: 3, tipo: 2, icono: "mdi-cash-minus", titulo: "Cuentas por pagar" },
+  {
+    tipo: 1,
+    titulo: "Modulo de ventas",
+    color: "amber darken-1",
+  },
+  { id: 4, tipo: 2, icono: "mdi-cash-plus", titulo: "Vender" },
+  { id: 5, tipo: 2, icono: "mdi-account-group", titulo: "Clientes" },
+  { id: 6, tipo: 2, icono: "mdi-cash-plus", titulo: "Cuentas por cobrar" },
+];
