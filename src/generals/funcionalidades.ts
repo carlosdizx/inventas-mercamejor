303e9a08c47202d4cef22a601e6c6c65e9c41a05
Carlos <carlodiaz@umariana.edu.co>
2022-03-20 00:20:52 -0500

    Definido algunos de los modulos que se usarán dentro del aplicativo

--- src/generals/funcionalidades.ts
+++ src/generals/funcionalidades.ts
@@ -4,7 +4,7 @@ export const FUNCIONES: any[] = [
     titulo: "Modulo de compras",
     color: "success darken-1",
   },
-  { id: 1, tipo: 2, icono: "mdi-cart-arrow-up", titulo: "Compras" },
+  { id: 1, tipo: 2, icono: "mdi-cart-plus", titulo: "Comprar" },
   { id: 2, tipo: 2, icono: "mdi-account-group", titulo: "Proveedores" },
   { id: 3, tipo: 2, icono: "mdi-cash-minus", titulo: "Cuentas por pagar" },
   {
@@ -15,4 +15,22 @@ export const FUNCIONES: any[] = [
   { id: 4, tipo: 2, icono: "mdi-cash-plus", titulo: "Vender" },
   { id: 5, tipo: 2, icono: "mdi-account-group", titulo: "Clientes" },
   { id: 6, tipo: 2, icono: "mdi-cash-plus", titulo: "Cuentas por cobrar" },
+  {
+    tipo: 1,
+    titulo: "Modulo de inventario",
+    color: "red darken-1",
+  },
+  { id: 7, tipo: 2, icono: "mdi-shape-plus", titulo: "Productos" },
+  { id: 7, tipo: 2, icono: "mdi-warehouse", titulo: "Bodegas" },
+  {
+    tipo: 1,
+    titulo: "Modulo de usuarios",
+    color: "indigo darken-1",
+  },
+  {
+    id: 7,
+    tipo: 2,
+    icono: "mdi-account-multiple",
+    titulo: "Empleados",
+  },
 ];
