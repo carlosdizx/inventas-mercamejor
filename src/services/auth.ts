e8b31818ce441118eb8c2babeced88d1ea50b994
dacho7 <davidtobarst@gmail.com>
2022-03-21 02:11:47 -0500

    Primera implementacion login sin validacion

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,17 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { AUTH } from "@/firebase/config";
 
 export const CREAR_CUENTA = async (correo: string, password: string) =>
   await createUserWithEmailAndPassword(AUTH, correo, password);
+
+export const LOGIN = async (email: string, password: string) => {
+  try {
+    const res = await signInWithEmailAndPassword(AUTH, email, password);
+    return res ? res.user.uid : "null";
+  } catch (error) {
+    console.log(error);
+  }
+};
