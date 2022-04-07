ad7ee9a12b3c8d971bd8c2520222a81a62805d9d
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2022-04-07 01:39:55 -0500

    Agregado nuevo campo

--- src/models/Bodegas/ColumnasBodega.ts
+++ src/models/Bodegas/ColumnasBodega.ts
@@ -1,5 +1,6 @@
 export const COLUMNAS = [
   { text: "Detalle", value: "detalle" },
   { text: "Nombre", value: "nombre" },
+  { text: "Dirección", value: "direccion" },
   { text: "Acciones", value: "acciones" },
 ];
--- src/models/Bodegas/FormBodega.ts
+++ src/models/Bodegas/FormBodega.ts
@@ -8,4 +8,13 @@ export const CAMPOS = [
     model: "",
     rules: "required|min:1|max:50",
   },
+  {
+    label: "Dirección",
+    prepend_icon: "mdi-home",
+    type: 1,
+    format: "text",
+    name: "direccion",
+    model: "",
+    rules: "required|min:2|max:70",
+  },
 ];
