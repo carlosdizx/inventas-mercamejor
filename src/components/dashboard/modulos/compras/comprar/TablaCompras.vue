a8777155442903891789d0e7c7681bf9471e60ac
dacho7 <davidtobarst@gmail.com>
2022-05-03 17:41:20 -0500

    Validaciones agregar nuevo producto

--- src/components/dashboard/modulos/compras/comprar/TablaCompras.vue
+++ src/components/dashboard/modulos/compras/comprar/TablaCompras.vue
@@ -41,29 +41,41 @@
                 <v-text-field
                   @input="calcularSubtotal()"
                   v-model="productoNuevo.cantidad"
+                  type="number"
                 ></v-text-field>
               </td>
               <td>
                 <v-text-field
                   @input="calcularSubtotal()"
+                  type="number"
                   v-model="productoNuevo.precioCompra"
                 ></v-text-field>
               </td>
               <td>
                 <v-text-field
+                  @input="ingresarGanancia()"
+                  type="number"
                   v-model="productoNuevo.porGanancia"
                 ></v-text-field>
               </td>
               <td>
                 <v-text-field
+                  @input="ingresarVenta()"
+                  type="number"
                   v-model="productoNuevo.precioVenta"
                 ></v-text-field>
               </td>
               <td>
-                <v-text-field v-model="productoNuevo.impuesto"></v-text-field>
+                <v-text-field
+                  type="number"
+                  v-model="productoNuevo.impuesto"
+                ></v-text-field>
               </td>
               <td>
-                <v-text-field v-model="productoNuevo.descuento"></v-text-field>
+                <v-text-field
+                  type="number"
+                  v-model="productoNuevo.descuento"
+                ></v-text-field>
               </td>
               <td>
                 {{ productoNuevo.subTotal }}
@@ -72,7 +84,7 @@
                 <v-btn
                   color="white"
                   @click="agregarProducto"
-                  :disabled="!validarNuevoProducto"
+                  :disabled="!validarProd"
                   icon
                   class="warning ml-1"
                 >
@@ -84,8 +96,9 @@
               </td>
             </tr>
           </tbody>
+          {{ typeof productoNuevo.precioCompra }}
           <tbody class="pt-3">
-            <tr v-for="item in desserts" :key="item.name">
+            <tr v-for="(item, index) in desserts" :key="index">
               <td>{{ item.codigo }}</td>
               <td>{{ item.descripcion }}</td>
               <td>{{ item.bodega }}</td>
@@ -100,7 +113,12 @@
                 <v-btn color="white" icon class="success">
                   <v-icon color="white">mdi-lead-pencil</v-icon>
                 </v-btn>
-                <v-btn color="white" icon class="error ml-1">
+                <v-btn
+                  color="white"
+                  icon
+                  class="error ml-1"
+                  @click="eliminarItem(index)"
+                >
                   <v-icon>mdi-trash-can-outline</v-icon>
                 </v-btn>
               </td>
@@ -116,37 +134,12 @@
 import Vue from "vue";
 
 import { LISTAR_BODEGAS, LISTAR_PRODUCTOS } from "@/generals/Funciones";
-import { VALIDARCAMPOSNULOS } from "@/generals/procesamientos";
+import { REDONDEAR, VALIDARCAMPOSNULOS } from "@/generals/procesamientos";
 
 export default Vue.extend({
   name: "TablaCompras",
   data: () => ({
-    desserts: [
-      {
-        codigo: "9452342312",
-        descripcion: "yogurt griego",
-        bodega: "Princial",
-        cantidad: 12,
-        precioCompra: 5800,
-        precioVenta: 8200,
-        porGanancia: 0,
-        impuesto: 0,
-        descuento: 100,
-        subTotal: 70000,
-      },
-      {
-        codigo: "9452342321",
-        descripcion: "Ice cream sandwich",
-        bodega: "Princial",
-        cantidad: 10,
-        precioCompra: 8000,
-        precioVenta: 92000,
-        porGanancia: 12,
-        impuesto: 0,
-        descuento: 0,
-        subTotal: 80000,
-      },
-    ],
+    desserts: [""],
     productoNuevo: {
       codigo: "",
       descripcion: "",
@@ -154,10 +147,10 @@ export default Vue.extend({
       cantidad: 1,
       precioCompra: 0,
       precioVenta: 0,
-      porGanancia: 12,
+      porGanancia: 0,
       impuesto: 0,
       descuento: 0,
-      subTotal: 10,
+      subTotal: 0,
     },
     bodegas: [""],
     productos: [""],
@@ -166,13 +159,28 @@ export default Vue.extend({
     validarNuevoProducto() {
       return VALIDARCAMPOSNULOS(this.productoNuevo);
     },
+    validarProd() {
+      if (
+        !this.productoNuevo.codigo ||
+        !this.productoNuevo.descripcion ||
+        !this.productoNuevo.descripcion ||
+        !this.productoNuevo.bodega ||
+        this.productoNuevo.cantidad < 1 ||
+        this.productoNuevo.precioCompra < 1 ||
+        this.productoNuevo.precioVenta < 1 ||
+        this.productoNuevo.porGanancia < 0 ||
+        this.productoNuevo.subTotal < 1 ||
+        this.productoNuevo.precioVenta < this.productoNuevo.precioCompra
+      )
+        return false;
+      return true;
+    },
   },
   methods: {
     async listarBodegas() {
       this.bodegas = [];
       const res: any = await LISTAR_BODEGAS();
-      res.forEach((bod: any) => this.bodegas.push(bod.data()));
-      this.productoNuevo.bodega = this.bodegas[1];
+      res.forEach((bod: any) => this.bodegas.unshift(bod.data()));
     },
     async listarProductos() {
       this.productos = [];
@@ -180,8 +188,12 @@ export default Vue.extend({
       res.forEach((prod: any) => this.productos.push(prod.data()));
     },
     agregarProducto() {
-      this.desserts.unshift(this.productoNuevo);
+      const product: any = { ...this.productoNuevo };
+      this.desserts.unshift(product);
       this.resetNuevoProducto();
+    },
+    eliminarItem(index: number) {
+      this.desserts.splice(index, 1);
     },
     resetNuevoProducto() {
       this.productoNuevo.codigo = "";
@@ -189,26 +201,52 @@ export default Vue.extend({
       this.productoNuevo.cantidad = 1;
       this.productoNuevo.precioCompra = 0;
       this.productoNuevo.precioVenta = 0;
-      this.productoNuevo.porGanancia = 12;
       this.productoNuevo.impuesto = 0;
       this.productoNuevo.descuento = 0;
       this.productoNuevo.subTotal = 10;
     },
     buscarProducto() {
+      let producto = "";
       this.productos.forEach((prod: any) => {
         if (prod.codigo_barras === this.productoNuevo.codigo) {
-          this.productoNuevo.descripcion = prod.nombre;
+          producto = prod.nombre;
         }
       });
+      this.productoNuevo.descripcion = producto;
     },
     calcularSubtotal() {
-      this.productoNuevo.subTotal =
+      const subTotal: number =
         this.productoNuevo.cantidad * this.productoNuevo.precioCompra;
+      this.productoNuevo.subTotal = subTotal;
+    },
+    ingresarGanancia() {
+      if (this.productoNuevo.porGanancia >= 0) {
+        let precioVenta: number =
+          this.productoNuevo.precioCompra *
+          (1 + this.productoNuevo.porGanancia / 100);
+        let precio = REDONDEAR(precioVenta, -2);
+        this.productoNuevo.precioVenta = precio;
+      }
+    },
+    ingresarVenta() {
+      if (this.productoNuevo.precioVenta >= this.productoNuevo.precioCompra) {
+        const porGanancia: number =
+          ((this.productoNuevo.precioVenta - this.productoNuevo.precioCompra) /
+            this.productoNuevo.precioCompra) *
+          100;
+        this.productoNuevo.porGanancia = Math.trunc(porGanancia);
+      } else {
+        this.productoNuevo.porGanancia = 0;
+      }
     },
   },
   created() {
     this.listarBodegas();
     this.listarProductos();
+    console.log(this.productoNuevo.impuesto > 0);
+  },
+  mounted() {
+    this.desserts = [];
   },
 });
 </script>
