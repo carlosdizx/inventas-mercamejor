3409481c8c4ff5e6674b2681b193b61be5eecc6d
dacho7 <davidtobarst@gmail.com>
2022-05-05 20:24:30 -0500

    ordenar modulos en dashboard

--- src/generals/permisos.ts
+++ src/generals/permisos.ts
@@ -1,10 +1,12 @@
 export const PERMISOS: any[] = [
   {
     tipo: "Admin",
-    idDisponibles: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    idDisponibles: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+    ],
   },
   {
     tipo: "Empleado",
-    idDisponibles: [1, 2, 3, 4, 5, 6, 7, 8, 17],
+    idDisponibles: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   },
 ];
