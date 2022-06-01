3e1a2248c17dcbeed47e0a41f7851f260fa0cee4
Carlos Díaz <carlos.diaz@ias.com.co>
2022-06-01 18:35:52 -0500

    Acomplado logica de venta en un componente y subdividido a su vez en otros componentes

--- src/components/dashboard/modulos/ventas/Ventas.vue
+++ src/components/dashboard/modulos/ventas/Ventas.vue
@@ -1,24 +1,18 @@
 <template>
   <v-container>
-    <Tabla
-      coleccion="ventas"
-      titulo="Ventas"
-      :columnas="columnas"
-      no-crear
-      no-editar
-    />
+    <FormVentas />
+    <ListadoVue />
   </v-container>
 </template>
 
 <script lang="ts">
-import Tabla from "@/components/crud/Tabla.vue";
+import FormVentas from "@/components/dashboard/modulos/ventas/SubComponents/FormVentas.vue";
+import ListadoVue from "@/components/dashboard/modulos/ventas/SubComponents/LsitadoVentas.vue";
 import Vue from "vue";
 
 export default Vue.extend({
   name: "Ventas",
-  components: {
-    Tabla,
-  },
+  components: { FormVentas, ListadoVue },
   data: () => ({
     columnas: [],
   }),
