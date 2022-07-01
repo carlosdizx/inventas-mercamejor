58b1e6b8d8228da0411078ec30eb1832d7595791
dacho7 <davidtobarst@gmail.com>
2022-07-01 18:40:17 -0500

    Modelo cuentas por pagar

---
+++ src/models/CuentasPorPagar.ts
@@ -0,0 +1,6 @@
+export const COLUMNAS = [
+  { text: "Documento Proveedor", value: "documento_proveedor" },
+  { text: "Nombre Proveedor", value: "nombre_proveedor" },
+  { text: "Valor", value: "total" },
+  { text: "Código de factura", value: "cod_factura" },
+];
