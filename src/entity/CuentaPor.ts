194a783641e99e035d3d2f112279121785915def
Carlos Díaz <carlos.diaz@ias.com.co>
2022-07-22 13:25:57 -0500

    agregado algunos metodos con logica propia para ayudar a hacer consultas

--- src/entity/CuentaPor.ts
+++ src/entity/CuentaPor.ts
@@ -3,21 +3,28 @@ import { AbonoCuentas } from "@/dto/AbonoCuentas";
 import { Cliente } from "@/entity/Cliente";
 import { Producto } from "@/entity/Producto";
 
+export const TIPO_COBRAR = "por cobrar";
+export const TIPO_PAGAR = "por pagar";
 export class CuentaPor {
-  private _factura: Factura;
-  private _cliente: Cliente | Producto;
-  private _consecutivo: string;
-  private _abonos: AbonoCuentas[];
-  private _fecha: Date;
-  private _tipo: string;
+  private readonly _factura: Factura;
+  private readonly _cliente: Cliente | Producto;
+  private readonly _consecutivo: string;
+  private readonly _abonos: AbonoCuentas[];
+  private readonly _fecha: Date;
+  private readonly _tipo: string;
 
-  constructor(factura: Factura, cliente: Cliente, consecutivo: string) {
+  constructor(
+    factura: Factura,
+    cliente: Cliente,
+    consecutivo: string,
+    tipo: number
+  ) {
     this._factura = factura;
     this._cliente = cliente;
     this._consecutivo = consecutivo;
     this._abonos = [];
     this._fecha = new Date();
-    this._tipo = "por cobrar";
+    this._tipo = tipo === 1 ? TIPO_COBRAR : TIPO_PAGAR;
   }
 
   get factura(): Factura {
@@ -42,5 +49,27 @@ export class CuentaPor {
 
   get tipo(): string {
     return this._tipo;
+  }
+
+  agregarAbono(abono: AbonoCuentas): void {
+    this._abonos.push(abono);
+  }
+
+  darTotalAbonos(): number {
+    let total = 0;
+    this._abonos.forEach((abono) => (total += abono.valor));
+    return total;
+  }
+
+  darTotalDeuda(): string {
+    let msg;
+    if (this._tipo === TIPO_COBRAR) {
+      msg = `El cliente adeuda: ${this._factura.total - this.darTotalAbonos()}`;
+    } else {
+      msg = `Al proveedor se le adeuda: ${
+        this._factura.total - this.darTotalAbonos()
+      }`;
+    }
+    return msg;
   }
 }
