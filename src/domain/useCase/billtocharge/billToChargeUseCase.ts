423d6106feabb8b6a38daf5ba749dd34b2c92030
dacho7 <davidtobarst@gmail.com>
2023-12-26 21:51:13 -0500

    consult balance by client

--- src/domain/useCase/billtocharge/billToChargeUseCase.ts
+++ src/domain/useCase/billtocharge/billToChargeUseCase.ts
@@ -2,10 +2,21 @@ import { Sale } from "@/domain/model/sale/Sale";
 import {
   SAVE_NEW_BILL_TO_CHARGE,
   PAYMENTE_BILL_TO_CHARGE,
+  FIND_BILL_TO_CHARGE,
 } from "@/infrastructure/firebase/adapter/billtocharge/billToChargeAdapter";
 
 export const SAVE_BILL_TO_CHARGE = async (sale: Sale): Promise<void> => {
   return SAVE_NEW_BILL_TO_CHARGE(sale);
+};
+
+export const CONSULT_BALANCE_BILL_TO_PAY = async (
+  docClient: string
+): Promise<number> => {
+  const consult = await FIND_BILL_TO_CHARGE(docClient);
+  if (consult != null) {
+    return consult.value;
+  }
+  return 0;
 };
 
 export const PAYMENT_BILL_TO_PAY = async (
