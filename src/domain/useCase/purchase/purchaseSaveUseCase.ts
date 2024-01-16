9e821974fc9af804ae05be3fa8799b405c96b72a
dacho7 <davidtobarst@gmail.com>
2024-01-16 18:33:55 -0500

    disabled inventory and updated unit product

--- src/domain/useCase/purchase/purchaseSaveUseCase.ts
+++ src/domain/useCase/purchase/purchaseSaveUseCase.ts
@@ -3,9 +3,9 @@ import {
   SAVE_PURCHASE,
   UPDATE_PURCHASE,
 } from "@/infrastructure/firebase/adapter/purchase/purchaseAdapter";
-import { SAVE_INVENTORY_FROM_PURCHASE } from "../inventory/inventoryUseCase";
+//import { SAVE_INVENTORY_FROM_PURCHASE } from "../inventory/inventoryUseCase";
 import { SAVE_BILL_TO_PAY } from "../billtopay/billToPayUseCase";
-import { UNITS_UPDATED_FROM_PURCHASE } from "../product/productUseCase";
+//import { UNITS_UPDATED_FROM_PURCHASE } from "../product/productUseCase";
 import {
   EEstatePurchase,
   EPayTypePurchase,
@@ -16,11 +16,11 @@ export const REGISTER_NEW_PURCHASE = async (
   purchase: Purchase
 ): Promise<boolean> => {
   await SAVE_PURCHASE(purchase);
-  await SAVE_INVENTORY_FROM_PURCHASE(purchase);
+  //await SAVE_INVENTORY_FROM_PURCHASE(purchase);
   if (purchase.type_pay === EPayTypePurchase.CREDITO) {
     await SAVE_BILL_TO_PAY(purchase);
   }
-  await UNITS_UPDATED_FROM_PURCHASE(purchase);
+  //await UNITS_UPDATED_FROM_PURCHASE(purchase);
   return true;
 };
 
