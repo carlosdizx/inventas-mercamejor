b64b155c1343b9a451b30ddd45dd27a19f5f9967
dacho7 <davidtobarst@gmail.com>
2024-10-28 22:48:33 -0500

    save sale with "f1" key

--- src/components/dashboard/modules/sales/Sales.vue
+++ src/components/dashboard/modules/sales/Sales.vue
@@ -6,13 +6,14 @@
           v-on:codigo_barras="buscarProducto($event)"
           v-on:wihtout_product_register="registerProduct($event)"
           v-on:datos_cliente="generarFactura($event)"
+          v-on:save_sale_without_factura="saveSaleWithoutInvoice($event)"
           ref="SalesForm"
         />
       </v-col>
       <v-col cols="12" md="6">
         <v-row>
           <v-col>
-            <ItemsList ref="ItemsList" @reset_venta="focusBarcodeField" />
+            <ItemsList ref="ItemsList" />
           </v-col>
         </v-row>
       </v-col>
@@ -120,6 +121,57 @@ export default Vue.extend({
         });
       }
     },
+    async saveSaleWithoutInvoice(sale: Sale) {
+      const datos: any = this.$refs.ItemsList;
+      const formVentas: any = this.$refs.SalesForm;
+      const productos: ProductSale[] = datos.darItemsFactura().productos;
+      const newProducts: ProductSale[] = productos.map((e: ProductSale) => {
+        return {
+          bar_code: e.bar_code,
+          name: e.name,
+          amount: e.amount,
+          shop_price: e.shop_price,
+          sale_price: e.sale_price,
+          taxes: 0,
+          discount: 0,
+          subtotal: e.subtotal,
+        } as ProductSale;
+      });
+      if (productos.length > 0) {
+        sale.sales = [...newProducts];
+        let subtotal = 0;
+        let descuento = 0;
+        let total = 0;
+        for (const temp of productos) {
+          subtotal += temp.subtotal;
+          descuento += temp.discount * temp.amount;
+          total = subtotal - descuento;
+        }
+        sale.total = total;
+        sale.subtotal = total;
+        await REGISTER_NEW_SALE({ ...sale });
+        const consecutivo = await DAR_NUMERO_FACTURA(1);
+        if (typeof consecutivo === "boolean") {
+          return;
+        }
+        datos.resetValues();
+        formVentas.resetDatosVenta();
+        await Swal.fire({
+          title: "Registro exitoso",
+          icon: "success",
+          timer: 500,
+          showConfirmButton: false,
+        });
+      } else {
+        await Swal.fire({
+          title: "Sin productos",
+          html: "No se puede generar una factura y registrar venta",
+          icon: "error",
+          timer: 800,
+          showConfirmButton: false,
+        });
+      }
+    },
     print(sale: Sale) {
       const ventanaImpresion = window.open("", "_blank");
       if (ventanaImpresion) {
@@ -137,10 +189,6 @@ export default Vue.extend({
       } else {
         console.error("No se pudo abrir la ventana de impresión");
       }
-    },
-    focusBarcodeField() {
-      const formVentas: any = this.$refs.SalesForm;
-      formVentas.focusBarcodeField();
     },
   },
   async created() {
